d2ad7675deb97777d376916d5eb0ffc70f692561
Alicia Barbosa <aliciabarbosa990@gmail.com>
2023-03-07 20:51:42 -0300

    implementação da soma da calculadora

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,13 +3,12 @@ function somar() {
   const numero2 = parseFloat(document.getElementById("numero2Soma").value);
   // evite mexer no código acima!
 
-
-
+const resultado = numero1 + numero2;
 
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
   Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
-  document.getElementById("resultadoSoma").innerHTML = 10;
+  document.getElementById("resultadoSoma").innerHTML = resultado;
 }
 
 function subtrair() {
