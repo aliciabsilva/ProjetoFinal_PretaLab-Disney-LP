b3bba499e9d6d57cdc1360869f08e95c4a4574a5
Alicia Barbosa <aliciabarbosa990@gmail.com>
2023-03-07 21:27:15 -0300

    implementação da divisão da calculadora

--- js/calculadora.js
+++ js/calculadora.js
@@ -44,11 +44,11 @@ function dividir() {
   const numero2 = parseFloat(document.getElementById("numero2Div").value);
   // evite mexer no código acima!
 
-
+const resultado = numero1 / numero2;
 
 
   
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero -89, mostre o resultado da operação */
-  document.getElementById("resultadoDiv").innerHTML = -89;
+  document.getElementById("resultadoDiv").innerHTML = resultado;
 }
